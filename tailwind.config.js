1747b47177f9ad95d5bc516d91f24c5e4d4e6339
Dathatreya14 <150936336+Dathatreya14@users.noreply.github.com>
2025-08-09 14:17:00 +0530

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        brand: "#6366F1",
+        darkBg: "#0f0f10",
+      },
+    },
+  },
+  plugins: [],
+};
